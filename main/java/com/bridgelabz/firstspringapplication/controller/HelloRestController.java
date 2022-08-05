5d506b812d459036e27c034510a856d8f4114043
MANOJ-51 <m4anoj51@gmail.com>
2022-08-05 17:18:52 +0530

    added: method to display name

--- main/java/com/bridgelabz/firstspringapplication/controller/HelloRestController.java
+++ main/java/com/bridgelabz/firstspringapplication/controller/HelloRestController.java
@@ -1,14 +1,20 @@
 package com.bridgelabz.firstspringapplication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloRestController {
+    //uc :1 display hello message //http://localhost:8080/hello/home
     @GetMapping("home")
     public String sayHello() {
         return "Hello Manoj";
     }
+
+    //uc2 :2 display name  //http://localhost:8080/hello/name?name=manoj
+    @GetMapping("name")
+    public String displayName(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
 }
